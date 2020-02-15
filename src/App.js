fcf7a3989bb94d665d219c26f385d064cdd1201a
wbc <417780894@qq.com>
2020-02-16 00:43:16 +0800

    composition 组件复合，相当于vue中的插槽

--- src/App.js
+++ src/App.js
@@ -3,13 +3,15 @@ import React from 'react';
 import JsxTest from './components/JsxTest';
 import ContextTest from './components/ContextTest';
 import HocTest from './components/HocTest';
+import Composition from './components/Composition';
 
 export default function App() {
   return (
     <div>
       {/* <JsxTest/> */}
       {/* <ContextTest/> */}
-      {<HocTest/>}
+      {/* {<HocTest/>} */}
+      {<Composition/>}
     </div>
   )
 }
